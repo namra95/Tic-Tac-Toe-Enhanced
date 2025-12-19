a6636a8e277d1f99b2e043c5f799004294553579
Namra Zubair <namra.z@outlook.com>
2025-12-19 00:03:07 -0600

    added Render health checks

---
+++ src/main/java/ttt/api/RootController.java
@@ -0,0 +1,19 @@
+package ttt.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "OK";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+}
+
